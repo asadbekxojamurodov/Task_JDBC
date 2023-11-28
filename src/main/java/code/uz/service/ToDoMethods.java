ce0e3671a170adba763febc67a22cd88e2f9d96d
asadbek <asadbekxojamurodov68@gmail.com>
2023-11-28 16:41:48 +0500

    Third commit

--- src/main/java/code/uz/service/ToDoMethods.java
+++ src/main/java/code/uz/service/ToDoMethods.java
@@ -3,21 +3,18 @@ package code.uz.service;
 import code.uz.model.Status;
 import code.uz.model.Todos;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class ToDoMethods {
     static Scanner scanner = new Scanner(System.in);
+    static Scanner scannerInt = new Scanner(System.in);
     static Todos todos = new Todos();
     static Connection con = null;
-
-
-
-
 
     public static void enterVariables() {
         System.out.println("Enter title");
@@ -40,7 +37,7 @@ public class ToDoMethods {
             Statement statement = con.createStatement();
 
             String sql = "insert into todos(title,content,status,created_date) values('%s','%s','%s','%s')";
-            sql = String.format(sql, dto.getTitle(), dto.getContent(),Status.ACTIVE,dto.getCreated_date() );
+            sql = String.format(sql, dto.getTitle(), dto.getContent(), Status.ACTIVE, dto.getCreated_date());
 
             int effectedRows = statement.executeUpdate(sql);
             System.out.println("effectedRows = " + effectedRows);
@@ -60,5 +57,165 @@ public class ToDoMethods {
             }
         }
     }
+
+    public static void variableForUpdate() {
+        System.out.println("Enter title");
+        String title = scanner.nextLine();
+        System.out.println("Enter content");
+        String content = scanner.nextLine();
+        System.out.println("Enter id");
+        int id = scannerInt.nextInt();
+        todos.setTitle(title);
+        todos.setContent(content);
+        update(id, todos);
+    }
+
+    public static List<Todos> getActiveTodos() {
+
+        List<Todos> todosList = new LinkedList<>();
+        String url = "jdbc:postgresql://localhost:5432/db_lesson_jon";
+        Properties info = new Properties();
+        info.put("user", "asadbek");
+        info.put("password", "1");
+
+
+        try {
+            con = DriverManager.getConnection(url, info);
+
+//            Statement statement = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            Statement statement = con.createStatement();
+
+            ResultSet res = statement.executeQuery("select * from todos where status = '" + Status.ACTIVE + "' ");
+            while (res.next()) {
+                Todos todo = new Todos();
+                todo.setId(res.getInt("id"));
+                todo.setTitle(res.getString("title"));
+                todo.setContent(res.getString("content"));
+                todo.setStatus(Status.valueOf(res.getString("status")));
+                todo.setCreated_date(res.getTimestamp("created_date").toLocalDateTime());
+                todo.setFinished_date(res.getTimestamp("finished_date"));
+                todosList.add(todo);
+            }
+
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return todosList;
+
+    }
+
+    public static List<Todos> getFinishedTodolist() {
+        List<Todos> todosList = new LinkedList<>();
+        String url = "jdbc:postgresql://localhost:5432/db_lesson_jon";
+        Properties info = new Properties();
+        info.put("user", "asadbek");
+        info.put("password", "1");
+
+
+        try {
+            con = DriverManager.getConnection(url, info);
+
+//            Statement statement = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            Statement statement = con.createStatement();
+
+            ResultSet res = statement.executeQuery("select * from todos where status = '" + Status.DONE + "' ");
+            while (res.next()) {
+                Todos todo = new Todos();
+                todo.setId(res.getInt("id"));
+                todo.setTitle(res.getString("title"));
+                todo.setContent(res.getString("content"));
+                todo.setStatus(Status.valueOf(res.getString("status")));
+                todo.setCreated_date(res.getTimestamp("created_date").toLocalDateTime());
+                todo.setFinished_date(res.getTimestamp("finished_date"));
+                todosList.add(todo);
+            }
+
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return todosList;
+
+    }
+
+    public static void update(int id, Todos dto) {
+        try {
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/db_lesson_jon",
+                    "asadbek", "1");
+
+            Statement statement = con.createStatement();
+
+            String sql = "update todos set title = '%s' , content = '%s' where id = " + id;
+            sql = String.format(sql, dto.getTitle(), dto.getContent(), id);
+
+            int effectedRows = statement.executeUpdate(sql);
+            if (effectedRows > 0) {
+                System.out.println("successfully updated");
+            } else {
+                System.out.println("todo not found");
+            }
+            con.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void delete(int id) {
+        try {
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/db_lesson_jon",
+                    "asadbek", "1");
+
+            Statement statement = con.createStatement();
+
+            String sql = "delete from todos where id = " + id;
+
+            int effectedRows = statement.executeUpdate(sql);
+            if (effectedRows > 0) {
+                System.out.println("successfully deleted");
+            } else {
+                System.out.println("todo not found");
+            }
+            con.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int requestId() {
+        System.out.println("Enter id");
+        int id = scannerInt.nextInt();
+        return id;
+    }
+
+    public static void mark() {
+        todos.setStatus(Status.DONE);
+        System.out.println("Enter id");
+        int id = scannerInt.nextInt();
+        markAsDone(id, todos);
+    }
+
+    public static void markAsDone(int id, Todos dto) {
+        try {
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/db_lesson_jon",
+                    "asadbek", "1");
+
+            Statement statement = con.createStatement();
+
+            String sql = "update todos set status = '%s'  where   id = " + id;
+            sql = String.format(sql, dto.getStatus(), id);
+            int effectedRows = statement.executeUpdate(sql);
+            if (effectedRows > 0) {
+                System.out.println("status changed successfully");
+
+            } else {
+                System.out.println("todo not found");
+            }
+            con.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
--- src/main/java/code/uz/ui/Main.java
+++ src/main/java/code/uz/ui/Main.java
@@ -1,130 +1,14 @@
 package code.uz.ui;
 
-import code.uz.model.Status;
-import code.uz.model.Todos;
-
-import java.sql.*;
-import java.time.LocalDateTime;
-import java.util.*;
+import code.uz.service.Service;
 
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
-    static Todos todos = new Todos();
-    static Connection con = null;
+    static Service service = new Service();
 
     public static void main(String[] args) throws ClassNotFoundException {
 
-        Class.forName("org.postgresql.Driver");
+        service.run();
 
-        do {
-            System.out.println("""
-                    
-                    1) Create todo
-                    2) Active todo list
-                    3) Finished todo list
-                    4) Update
-                    5) Delete
-                    6) Mark as done
-                    0) Exit
-                       """);
-            String option = scanner.nextLine();
-            if (option.equals("0")) {
-                System.out.println("Task completed");
-                return;
-            }
-            switch (option) {
-                case "1" -> {
-                    enterVariables();
-                    createTask(todos);
-                }
-                case "2" -> getActiveTodos().forEach(System.out::println);
-
-                case "3" -> System.out.println();
-                case "4" -> System.out.println();
-                case "5" -> System.out.println();
-                case "6" -> System.out.println();
-            }
-
-        } while (true);
 
     }
-
-    private static void enterVariables() {
-        System.out.println("Enter title");
-        String title = scanner.nextLine();
-        System.out.println("Enter content");
-        String content = scanner.nextLine();
-        todos.setTitle(title);
-        todos.setContent(content);
-        todos.setStatus(Status.ACTIVE);
-        todos.setCreated_date(LocalDateTime.parse(LocalDateTime.now().toString()));
-    }
-
-
-    public static void createTask(Todos dto) {
-
-        try {
-            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/db_lesson_jon",
-                    "asadbek", "1");
-
-            Statement statement = con.createStatement();
-
-            String sql = "insert into todos(title,content,status,created_date) values('%s','%s','%s','%s')";
-            sql = String.format(sql, dto.getTitle(), dto.getContent(), Status.ACTIVE, dto.getCreated_date());
-
-            int effectedRows = statement.executeUpdate(sql);
-            System.out.println("effectedRows = " + effectedRows);
-
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-
-            try {
-                if (con != null) {
-                    System.out.println("new task was created✏️✏️✏️✏️✏️");
-                    con.close();
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    public static List<Todos> getActiveTodos() {
-
-        List<Todos> todosList = new LinkedList<>();
-        String url = "jdbc:postgresql://localhost:5432/db_lesson_jon";
-        Properties info = new Properties();
-        info.put("user", "asadbek");
-        info.put("password", "1");
-
-
-        try {
-            con = DriverManager.getConnection(url, info);
-
-//            Statement statement = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            Statement statement = con.createStatement();
-
-            ResultSet res = statement.executeQuery("select * from todos ");
-            while (res.next()) {
-                Todos todo = new Todos();
-                todo.setId(res.getInt("id"));
-                todo.setTitle(res.getString("title"));
-                todo.setContent(res.getString("content"));
-                todo.setStatus(Status.valueOf(res.getString("status")));
-                todo.setCreated_date(res.getTimestamp("created_date").toLocalDateTime());
-                todo.setFinished_date(res.getTimestamp("finished_date"));
-                todosList.add(todo);
-            }
-
-            con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return todosList;
-
-    }
-
-
 }
